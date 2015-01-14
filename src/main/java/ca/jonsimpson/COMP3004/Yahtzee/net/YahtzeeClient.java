c00eed84c8de0657a1837da78af1bb90f827fdf6
Jon Simpson <jon@jonsimpson.ca>
2015-01-14 00:35:11 -0500

    - Add logging to YahtzeeClient - Move main methods contents into constructor

--- src/main/java/ca/jonsimpson/COMP3004/Yahtzee/net/YahtzeeClient.java
+++ src/main/java/ca/jonsimpson/COMP3004/Yahtzee/net/YahtzeeClient.java
@@ -1,23 +1,45 @@
 package ca.jonsimpson.COMP3004.Yahtzee.net;
 
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Date;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public class YahtzeeClient {
+	
+	private Log log = LogFactory.getLog(YahtzeeClient.class);
 	
 	private static final String RMI_SERVER_HOSTNAME = "localhost";
 	private static final int RMI_SERVER_PORT = 1099;
 
-	public static void main(String[] args) throws Exception {
+	public YahtzeeClient() {
+		log.info("Connecting to the server");
 		
-		// try connecting to the remote registry
-		Registry registry = LocateRegistry.getRegistry(RMI_SERVER_HOSTNAME, RMI_SERVER_PORT);
+		try {
+			// try connecting to the remote registry
+			Registry registry = LocateRegistry.getRegistry(RMI_SERVER_HOSTNAME, RMI_SERVER_PORT);
+
+			// get the CommandService interface from the registry
+			CommandService service = (CommandService) registry.lookup(CommandService.LOOKUPNAME);
+			log.info("Successfully connected to the server");
+			
+			Date today = service.getRemoteDate();
+			
+			System.out.println(today);
+			
+		} catch (RemoteException e) {
+			log.error("Unable to connect to the server", e);
+		} catch (NotBoundException e) {
+			log.error("Unable to connect to the server", e);
+		}
 		
-		// get the CommandService interface from the registry
-		CommandService service = (CommandService) registry.lookup(CommandService.LOOKUPNAME);
-		Date today = service.getRemoteDate();
-		
-		System.out.println(today);
+	}
+	
+	public static void main(String[] args) {
+		new YahtzeeClient();
 	}
 }
